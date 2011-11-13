54c4038e86cbeccf74a14f4b4ad16c641375d79f
antonishen <joshua@antonishen.com>
2011-11-13 13:13:25 -0500

    methods for early algo

--- lib/earley.rb
+++ lib/earley.rb
@@ -1,7 +1,23 @@
+require 'json'
+
 module Earley
   extend self
 
   def check_string(grammar_file, string)
+    json_file = File.read(grammar_file)
+    data = JSON.parse(json_file, :symbolize_names => true)
+  end
+
+  def earley_algo(chart, grammar)
+  end
+
+  def predict(chart, grammar)
+  end
+
+  def scan(chart, grammar)
+  end
+
+  def complete(chart, grammar)
   end
 
 end
