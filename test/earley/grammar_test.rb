677509a608431080bc47a6fc81ee1895592808c1
antonishen <joshua@antonishen.com>
2011-11-13 11:05:59 -0500

    moved raw data creation out of setup function

--- test/earley/grammar_test.rb
+++ test/earley/grammar_test.rb
@@ -2,24 +2,22 @@ require 'minitest/autorun'
 require_relative '../../lib/earley/grammar'
 
 class GrammarTest < MiniTest::Unit::TestCase
-  def setup
-    @raw_grammar = {
-                      rules: {
-                        Det: [["the"]],
-                        N: [["window"]],
-                        NP: [["ProperNoun"], ["Det", "N"]],
-                        PP: [["Prep", "NP"]],
-                        Prep: [["through"]],
-                        ProperNoun: [["Jen"], ["Bill"]],
-                        S: [["NP", "VP"]], :V=>[["saw"]],
-                        VP: [["V", "NP"], ["VP", "PP"]]
-                      },
-                      terminals: ["Jen", "Bill", "saw", "through", "the", "window"]
-                   }
-  end
-
   def test_initialize_new_grammar
-    grammar = Grammar.new(@raw_grammar)
+    raw_grammar = {
+                  rules: {
+                    Det: [["the"]],
+                    N: [["window"]],
+                    NP: [["ProperNoun"], ["Det", "N"]],
+                    PP: [["Prep", "NP"]],
+                    Prep: [["through"]],
+                    ProperNoun: [["Jen"], ["Bill"]],
+                    S: [["NP", "VP"]], :V=>[["saw"]],
+                    VP: [["V", "NP"], ["VP", "PP"]]
+                  },
+                  terminals: ["Jen", "Bill", "saw", "through", "the", "window"]
+               }
+
+    grammar = Grammar.new(raw_grammar)
     assert_equal @raw_grammar[:rules], grammar.rules
     assert_equal @raw_grammar[:terminals], grammar.terminals
   end
