c656d7f4546169bc098af7da613d6f01a5c4b61a
antonishen <joshua@antonishen.com>
2011-11-13 18:33:33 -0500

    More examples

--- program.rb
+++ program.rb
@@ -5,3 +5,6 @@ require_relative "lib/earley"
 puts Earley.check_string("./files/grammar1.json", "Jen saw Bill through the window")
 
 puts Earley.check_string("./files/grammar2.json", "Jen saw Bill")
+
+puts Earley.check_string("./files/grammar3.json", "Papa ate the caviar with a spoon")
+
